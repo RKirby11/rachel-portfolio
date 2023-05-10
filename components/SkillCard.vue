3dd76268238b5927ca697bf484d3fe0f84343f41
Rachel Kirby <rachel.kirby@outlook.com>
2023-05-10 09:55:51 +0100

    add invert in skills

--- components/SkillCard.vue
+++ components/SkillCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='relative flex flex-col flex-wrap items-center py-6' @mouseover="hover = true" @mouseleave="hover = false">
-        <nuxt-img :src='skill.icon' :alt='skill.skill' placeholder loading='lazy' class='h-12 w-fit' />
+        <nuxt-img :src='skill.icon' :alt='skill.skill' placeholder loading='lazy' class='h-12 w-fit' :class="['Wix', 'Neo4J', 'AWS', 'Express'].includes(skill.skill) ? 'invert dark:invert-0' : ''"/>
         <div v-if='hover' class='absolute top-8 h-5 w-32 mt-2 bg-dblue text-white dark:bg-dblue rounded-sm text-sm align-middle px-4'> {{ skill.skill }} </div>
     </div>
 </template>
